00a1c60a411bfa10f07f8c76021af277de3dbc40
mochoping <hodolskm@gmail.com>
2024-11-07 12:49:24 +0900

    241107조건문

--- 03_javascript/js/03_연산자과제1.js
+++ 03_javascript/js/03_연산자과제1.js
@@ -1,7 +1,11 @@
 const v1 = document.getElementById("n1");
 const v2 = document.getElementById("n2");
-const r1 = document.getElementById("res");
+const r0 = document.getElementById("res");
 
 function 계산함수() {
-  r1.innerText = v1.value + v2.value;
+  const r1 = v1.value + v2.value;
+  const r2 = v1.value * v2.value;
+  const r3 = v1.value - v2.value;
+
+  r0.innerText = r1.value + r2.value + r3.value;
 }
