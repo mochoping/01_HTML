9dbdcb448b653e0ca810dadcd93aac9ee8814a14
mochoping <hodolskm@gmail.com>
2024-11-14 17:49:50 +0900

    비활성화, 실습과제

--- 실습과제/1113_약관동의.js
+++ 실습과제/1113_약관동의.js
@@ -4,13 +4,22 @@ function selectCheck() {
   const result = document.getElementById("name-div");
   let count = 0;
   let total = " ";
- for( let i = 0; i < agreement.length; i++) {
-        if(!agreement[i].checked){
-          alert("모든 필수 약관에 동의해주세요.")
-          return;
-        }
+  for (let i = 0; i < agreement.length; i++) {
+    if (!agreement[i].checked) {
+      alert("모든 필수 약관에 동의해주세요.");
+      return;
     }
-
+  }
+  /**
+   * if(!service||!privacy){
+   * alert("모든 필수 약관에 동의해주세요")
+   * }
+   *
+   *
+   *
+   *
+   *
+   */
 
   for (let n = 0; n < hobby.length; n++) {
     if (hobby[n].checked) {
@@ -20,3 +29,15 @@ function selectCheck() {
     result.innerHTML = `${total} <br>선택된 취미 개수 : ${count}`;
   }
 }
+/**
+ * for문안에 결과가 있으면 출력전에 계속 루프를 돌 수 있음. 주의
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
